7ed61caedfb516721e02d2f8abe1d752f2453f7a
alexe <alexe@W10SSD>
2019-01-25 23:43:08 +0300

    JSX - синтаксис

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      <h1>Hello World!</h1>
+    // return (
+    //   <div className="App">
+    //   <h1>Hello World!</h1>
         
-      </div>
-    );
+    //   </div>
+    // );
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        null),
+      'Hello World!!!'
+    )
   }
 }
 
