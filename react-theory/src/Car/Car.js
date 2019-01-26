8384544a7b54918da7b1393a37f5c7e1779a05d3
alexe <alexe@W10SSD>
2019-01-26 15:33:34 +0300

    Можно использовать несколько строк

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -12,6 +12,12 @@ import React from 'react';
 //     )
 // }
 
-const car = () => <h2>This is car component</h2>
+const car = () => (
+<div>
+    This is car component
+    <strong>test</strong>
+
+</div>
+)
 
 export default car
