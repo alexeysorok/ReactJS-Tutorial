f1420258ab5eeccac88cbe4ee8c4042171cf19b7
alexe <alexe@W10SSD>
2019-01-26 13:35:53 +0300

    предпочтительный способ записи

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     const divStyle = {
-      'text-align' : 'center'
+      textAlign : 'center'
     }
     return (
       <div style={divStyle}>
-      <h1>Hello World!</h1>
+      <h1 style={{color: 'blue', fontSize: '30px'}} >Hello World!</h1>
         
       </div>
     );
