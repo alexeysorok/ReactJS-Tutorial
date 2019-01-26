2d1529499f339bad0909969fba1f804c093fa58d
alexe <alexe@W10SSD>
2019-01-26 16:24:09 +0300

    скобки можно пустить - новый синтаксис

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -22,7 +22,7 @@ import React from 'react';
 
 
 
-export default (props) => (
+export default props => (
     <div>
      <h3>Car name: {props.name}</h3>
      <p>Year: <strong>{props.year}</strong> </p>
